82e26ecb305db54def0ab7c7106381aedc9d9c4d
kamalesh0703 <kamaleshponmani0703@gmail.com>
2023-10-02 23:16:37 +0530

    first Commit

---
+++ src/Components/TodoApp.jsx
@@ -0,0 +1,130 @@
+import React, { Component } from "react";
+import { AddTodo, DeleteTodo,EditTodo,TodoComplete } from "../Services/TodoService";
+export default class TodoApplication extends Component {
+    constructor() {
+        super()
+        this.state = {
+            formdata: {
+                name: "",
+                title: "",
+                description: ""
+            },
+            TodoList: [],
+            isEdit: true
+        }
+        this.getTodoList = this.getTodoList.bind(this)
+    }
+    handleToggle = () => {
+        this.setState({ isEdit: false })
+    }
+    componentDidUpdate = () => {
+        this.getTodoList()
+    }
+    componentDidMount=()=>{
+        this.getTodoList()
+    }
+    getTodoList = async () => {
+        let resp = await fetch("http://localhost:5000/todo/getTodo")
+        let result = await resp.json()
+        this.setState({ TodoList: result })
+        this.setState({ isEdit: true })
+    }
+    handleChange = (event) => {
+        let name = event.target.name;
+        let value = event.target.value;
+        this.setState({
+            formdata: { ...this.state.formdata, [name]: value }
+        })
+    }
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.state.isEdit ? this.addTodo() : this.editTodo()
+    }
+    addTodo = async () => {
+        AddTodo(this.state.formdata)
+        this.formEmpty()
+        this.getTodoList()
+    }
+    handleDelete = async (id) => {
+        DeleteTodo(id)
+        this.getTodoList()
+    }
+    handleEdit = (Todo) => {
+        this.setState({
+            formdata: Todo
+        })
+        this.handleToggle()
+    }
+    editTodo = async () => {
+        let reqObj = {...this.state.formdata,status:"Todo"}
+        EditTodo(reqObj)
+        this.getTodoList()
+        this.formEmpty()
+    }
+    formEmpty = () => {
+        this.setState({
+            formdata: { name: "", description: "", title: "" }
+        })
+    }
+    handleComplete=async(Todo)=>{
+        TodoComplete(Todo)
+        this.getTodoList()
+    }
+    render() {
+        return (
+            <>
+                <h1 id="title">Todo App</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="container">
+                        <div className="row">
+                            <div id="inputfirst">
+                                <label>Name</label>
+                                <input type="text"
+                                    name="name"
+                                    value={this.state.formdata.name}
+                                    onChange={this.handleChange} />
+                            </div>
+                            <div id="inputthird">
+                                <label>Title</label>
+                                <input type="text"
+                                    name="title"
+                                    value={this.state.formdata.title}
+                                    onChange={this.handleChange} />
+                            </div>
+                            <div id="inputsecond">
+                                <label>Description</label>
+                                <input type="text"
+                                    name="description"
+                                    value={this.state.formdata.description}
+                                    onChange={this.handleChange} />
+                            </div>
+                        </div>
+                        <button type="submit" onSubmit={this.handleSubmit} id="addtodo">{this.state.isEdit ? 'Add Todo' : 'ReEdit'}</button>
+                    </div>
+                </form>
+                <div id="list-container">
+                    {
+                        this.state.TodoList.map((Todo, index) => {
+                            return (
+                                <>
+                                    <li key={Todo._id}>
+                                        <div>
+                                            <h3 style={{textDecoration:(Todo.status==="Complete")?"line-through":"none",color:(Todo.status==="Complete")?"gray":"#fe9901"}}>{Todo.name}</h3>
+                                            <h5 style={{textDecoration:(Todo.status==="Complete")?"line-through":"none",color:(Todo.status==="Complete")?"gray":"none"}}>{Todo.title}</h5>
+                                            <p style={{textDecoration:(Todo.status==="Complete")?"line-through":"none",color:(Todo.status==="Complete")?"gray":"none"}}>{Todo.description}</p>
+                                        </div>
+                                        <div>
+                                            <button id="complete" onClick={()=>{this.handleComplete(Todo)}} style={{display:(Todo.status==="Todo")?"":"none"}}>Complete</button>
+                                            <button id="edit" onClick={() => this.handleEdit(Todo)} style={{display:(Todo.status=== "Complete")? "none":""}}>Edit</button>
+                                            <button id="delete" onClick={() => this.handleDelete(Todo._id)}>Delete</button>
+                                        </div>
+                                    </li>
+                                </>
+                            )
+                        })
+                    }
+                </div>
+            </>
+        )
+    }
+}
---
+++ src/Services/TodoService.js
@@ -0,0 +1,46 @@
+export function AddTodo(payload){
+    let reqObj = {...payload,status:"Todo"}
+    console.log(reqObj)
+    fetch('http://localhost:5000/todo/addTodo', {
+            method: 'POST',
+            body: JSON.stringify(reqObj),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => {return json});
+}
+
+export async function DeleteTodo (id){
+    let header = {
+        method: "DELETE"
+    }
+    let resp = await fetch(`http://localhost:5000/todo/delete/${id}`, header)
+    let result = await resp.json({Status:"Delete Successfullly!"})
+    return result;
+}
+export async function EditTodo(reqObj){
+    let headers = {
+        method: "PUT",
+        body: JSON.stringify(reqObj),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }
+    let id=reqObj._id
+    fetch(`http://localhost:5000/todo/update/${id}`, headers)
+}
+
+export async function TodoComplete(contact){
+    let reqObj = {...contact,status:"Complete"}
+    console.log(reqObj)
+    let headers = {
+        method: "PUT",
+        body: JSON.stringify(reqObj),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }
+    fetch(`http://localhost:5000/todo/complete/${contact._id}`, headers)
+}
