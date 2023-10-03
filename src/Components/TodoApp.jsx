e6f0e32d3027f2bc0e348da75e57aca4eb8de6f3
kamalesh0703 <125384711+kamalesh0703@users.noreply.github.com>
2023-10-03 23:33:49 +0530

    Update TodoApp.jsx

--- src/Components/TodoApp.jsx
+++ src/Components/TodoApp.jsx
@@ -107,7 +107,7 @@ export default class TodoApplication extends Component {
                         this.state.TodoList.map((Todo, index) => {
                             return (
                                 <>
-                                    <li key={Todo._id}>
+                                    <li key={Todo._id} className="todolist">
                                         <div>
                                             <h3 style={{textDecoration:(Todo.status==="Complete")?"line-through":"none",color:(Todo.status==="Complete")?"gray":"#fe9901"}}>{Todo.name}</h3>
                                             <h5 style={{textDecoration:(Todo.status==="Complete")?"line-through":"none",color:(Todo.status==="Complete")?"gray":"none"}}>{Todo.title}</h5>
